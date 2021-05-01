0e9fd477e88aca879137e1172d19bb1cd33c2d10
Thiago <thiago.joaquim.ext@conductor.com.br>
2021-04-30 21:09:35 -0300

    Iniciando o projeto.

---
+++ src/main/kotlin/br/com/dcomp/ufs/idrug/IdrugApplication.kt
@@ -0,0 +1,11 @@
+package br.com.dcomp.ufs.idrug
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IdrugApplication
+
+fun main(args: Array<String>) {
+	runApplication<IdrugApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/dcomp/ufs/idrug/IdrugApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.dcomp.ufs.idrug
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IdrugApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
